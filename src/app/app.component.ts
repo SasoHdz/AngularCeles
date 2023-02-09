f59676817802393c1761e26a095c1449944f56c4
SasoHdz <isaacrdzcmd@gmail.com>
2023-02-09 17:52:41 -0600

    Practica 2, iniando con string interpolation y estructuras nf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'hola-mundo';
+  nombre = 'Isaac Hernández';
+  textPlaceholder="Escribe algo aquí";
+  texto="Esto es un video / boton sobre event binding"
+  texto2="";
+
+  //objeto
+  listEstudiantes: any[]= [
+    {no_control:"1914", nombreAlumno:"Juan Pablo",semestre:"VIII"},
+    {no_control:"8414", nombreAlumno:"Pedro Celestino",semestre:"VIII"},
+    {no_control:"0314", nombreAlumno:"Julio Cesar",semestre:"VIII"},
+  ]
+
+  constructor()
+  {
+    setInterval( ()=> this.nombre = 'Programación Web',3000);
+  }
+  //practica 2
+  cambiarText():void{
+    this.texto="en el proximo video vamos a ver two way data-binding";
+  }
+   mostrar = true;
+   toogle():void
+   {
+      this.mostrar =!this.mostrar
+   }
 }
